02512498469d017f625808bdde325b23b47ee7c8
Edu <eduardvf@outlook.com>
2022-08-20 23:22:47 +0200

    shortcuts added

--- std_lib.js
+++ std_lib.js
@@ -1,2 +1,30 @@
 // lluna lang js
 // std_lib.js - Standard Library
+
+const SHORTCUTS = {
+    // General
+    ':': 'var',
+    '~': 'func',
+    '?': 'ask', // if
+    '@': 'loop',
+    '->': 'say', // print
+    '<-': 'lsn', // input
+    '#': 'list',
+    '.': 'item',
+    // Logic
+    '!': 'not',
+    '&': 'and',
+    '|': 'or',
+    '=': 'eq',
+    '!=': 'neq',
+    '<': 'lt',
+    '<=': 'leq',
+    // Arithmetic
+    '+': 'add',
+    '-': 'sub',
+    '*': 'mul',
+    '/': 'div',
+    '%': 'mod',
+};
+
+module.exports = { SHORTCUTS };
