3ca656d5eee29c3102821361b3824372cf0ab861
EduVF <eduardvf@outlook.com>
2022-12-10 17:07:05 +0100

    Revert "small highligh fix"
    This reverts commit 1597fec5eb25df87c830bb69b5d3d99ff4aaadeb.

--- docs/script.js
+++ docs/script.js
@@ -1,46 +1,58 @@
-import { CodeJar } from "//unpkg.com/codejar";
+import { CodeJar } from '//unpkg.com/codejar';
 
-import lib from "./lluna/lib.mjs";
-import { exec } from "./lluna/lluna.mjs";
-import lluna from "./lluna/lluna.mjs";
+import lib from './lluna/lib.mjs';
+import { exec } from './lluna/lluna.mjs';
+import lluna from './lluna/lluna.mjs';
 
-const select = document.getElementById("select");
-const editor = document.querySelector(".editor");
-const output = document.getElementById("output");
+const select = document.getElementById('select');
+const editor = document.querySelector('.editor');
+const output = document.getElementById('output');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Code editor & syntax highlighting
 ////////////////////////////////////////////////////////////////////////////////
 
 const highlight = (editor) => {
-  let code = editor.textContent;
+	let code = editor.textContent;
 
-  code = code
-    .replace(/(\(\s*|^\s*)([^\s\(\)\,]+)/gm, '$1<span style="font-weight: bold; color: var(--orange);">$2</span>')
-    .replace(/(,[^\n]*$)/gm, '<span style="font-style: italic; color: var(--purple-lighter);">$1</span>')
-    .replace(/('.*?'|\d*\.\d+|\b\d+\.?)/g, '<span style="font-weight: bold;">$1</span>');
+	code = code
+		.replace(
+			/(\(\s*|^\s*)([^\s\(\)\,]+)/gm,
+			'$1<span style="font-weight: bold; color: var(--orange);">$2</span>'
+		)
+		.replace(
+			/('.*?'|\d*\.\d+|\b\d+\.?)/g,
+			'<span style="font-weight: bold;">$1</span>'
+		)
+		.replace(
+			/(,[^'\n]*$)/gm,
+			'<span style="font-style: italic; color: var(--purple-lighter);">$1</span>'
+		);
 
-  editor.innerHTML = code;
+	editor.innerHTML = code;
 };
 
 let jar = CodeJar(editor, highlight);
 // Set default message
-jar.updateCode(", start writing lluna code or select an example");
+jar.updateCode(', start writing lluna code or select an example');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Load example
 ////////////////////////////////////////////////////////////////////////////////
 
 window.loadExample = () => {
-  let scriptName = select.value;
-  let url = "https://raw.githubusercontent.com/eduvf/lluna-js/main/examples/" + scriptName + ".lluna";
+	let scriptName = select.value;
+	let url =
+		'https://raw.githubusercontent.com/eduvf/lluna-js/main/examples/' +
+		scriptName +
+		'.lluna';
 
-  // fetch code
-  fetch(url).then((response) => {
-    response.text().then((text) => {
-      jar.updateCode(text);
-    });
-  });
+	// fetch code
+	fetch(url).then((response) => {
+		response.text().then((text) => {
+			jar.updateCode(text);
+		});
+	});
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -48,10 +60,10 @@ window.loadExample = () => {
 ////////////////////////////////////////////////////////////////////////////////
 
 window.clearEditor = () => {
-  jar.updateCode("");
+	jar.updateCode('');
 };
 window.clearOutput = () => {
-  output.innerText = "(( lluna lang )) beta\nOutput:\n";
+	output.innerText = '(( lluna lang )) beta\nOutput:\n';
 };
 clearOutput();
 
@@ -61,13 +73,13 @@ clearOutput();
 
 // Modify print (>) to print to output
 let env = lib(exec);
-env[0][">"] = (arg, env) => {
-  let l = arg.map((a) => exec(a, env));
-  // console.log(l.join(' '));
-  output.innerText += "> " + l.join(" ") + "\n";
-  return l;
+env[0]['>'] = (arg, env) => {
+	let l = arg.map((a) => exec(a, env));
+	// console.log(l.join(' '));
+	output.innerText += '> ' + l.join(' ') + '\n';
+	return l;
 };
 window.runCode = () => {
-  output.innerText += "\n";
-  lluna(jar.toString(), env);
+	output.innerText += '\n';
+	lluna(jar.toString(), env);
 };
