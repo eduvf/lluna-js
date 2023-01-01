58ac6551b153422d086caab4d28a2495e6be158e
EduVF <eduardvf@outlook.com>
2023-01-02 00:33:16 +0100

    clean and comment lex() and half parse()

--- lluna.mjs
+++ lluna.mjs
@@ -4,127 +4,164 @@
  * auth: github.com/eduvf
  * desc: interpreter for the lluna programming language using JavaScript
  *
- * Structure of this file:
- * - lex()
- * - parse()
- * - exec()
- * - [export] lluna()
+ * this module is structured in four main functions:
+ *
+ * - lex(str)
+ * - parse(tok)
+ * - evaluate(node, env)
+ * - lluna(str, env)
+ *
+ * the last one is just a wrapper for the previous three
+ * IMPORTANT! you'll need the lib.mjs module too, imported down below
  */
 
 import lib from './lib.mjs';
 
 ////////////////////////////////////////////////////////////////
-/* LEX
- * the lexer takes a string of lluna coda as an argument and
- * returns an array of tokens, according to the lluna syntax
+/**
+ * LEXER
+ * takes a string containing lluna code and returns an array
+ * of tokens with the following structure:
+ * [
+ *     {type: 's', val: 'string', line: 1},
+ *     {type: 'i', val: 123, line: 2},
+ *     ...
+ * ]
+ *
+ * @param {String} str - string of lluna code
+ * @returns {Array} tok - list of tokens
  */
-
-function lex(s) {
-	// a symbol can be a number or a keyword
-	// and cannot include whitespace or certain characters
-	const SYMBOL = /[^\s,;()']/;
-
-	let tokens = [];
+function lex(str) {
+	// a symbol cannot be whitespace or a special char
+	const SYMBOL = /[^\s,;()[\]'"]/;
 	let line = 1;
+	let tok = []; // list of tokens
 
-	for (let i = 0; i < s.length; i++) {
-		let c = s[i]; // current character
-		if (c === ',') {
+	let i = 0;
+	while (i < str.length) {
+		let char = str[i]; // current character
+
+		if (char === ',') {
 			// comment
-			while (i + 1 < s.length && s[i + 1] !== '\n') i++;
-			line++;
-		} else if (';\n()'.includes(c)) {
+			while (i < str.length && str[i] !== '\n') i++;
+		} else if (';\n()[]'.includes(char)) {
 			// special characters
-			if (c === '\n') {
-				c = ';';
+			if (char === '\n') {
+				char = ';';
 				line++;
 			}
-			tokens.push(c);
-		} else if (`'"`.includes(c)) {
+			tok.push({ type: char, line: line });
+			i++;
+		} else if (`'"`.includes(char)) {
 			// string
-			let from = i;
+			i++;
+			let startLine = line;
+			let startI = i;
 			// advance until a non-escaped quote is found
-			for (i++; !(s[i - 1] !== '\\' && s[i] === c); i++) {
-				if (i >= s.length) {
-					throw `[!] Unclosed string starting at line ${from}.`;
-				} else if (s[i] === '\n') {
-					line++;
+			while (!(str[i - 1] !== '\\' && str[i] === char)) {
+				if (i >= str.length) {
+					throw `[!] Unclosed string starting at line ${startLine}.`;
 				}
+
+				if (str[i] === '\n') line++;
+				i++;
 			}
-			tokens.push({ type: 's', val: s.slice(from + 1, i), line: line });
-		} else if (SYMBOL.test(c)) {
+			tok.push({ type: 's', val: str.slice(startI, i), line: line });
+			i++; // ignore closing quote
+		} else if (SYMBOL.test(char)) {
 			// number or keyword
-			let sym = c;
-			for (; i + 1 < s.length && SYMBOL.test(s[i + 1]); i++) {
-				sym += s[i + 1];
+			let sym = char;
+			i++;
+			while (i < str.length && SYMBOL.test(str[i])) {
+				sym += str[i];
+				i++;
 			}
-			// check if it's a number. otherwise, it's a keyword
-			// as long as it doesn't start with a number
-			if (/^-?(\d+\.?|\d*\.\d+)$/.test(sym)) {
-				tokens.push({ type: 'n', val: Number(sym), line: line });
-			} else if (/[^\d]/.test(sym[0])) {
-				tokens.push({ type: 'k', val: sym, line: line });
+			// check if it's a number or keyword
+			if (/^-?\d+$/.test(sym)) {
+				tok.push({ type: 'i', val: Number(sym), line: line });
+			} else if (/^-?\d*\.\d+$/.test(sym)) {
+				tok.push({ type: 'f', val: Number(sym), line: line });
+			} else if (/\D/.test(sym[0])) {
+				tok.push({ type: 'k', val: sym, line: line });
 			} else {
 				throw `[!] Could not understand symbol '${sym}' at line ${line}.`;
 			}
+		} else {
+			// ignore whitespace
+			i++;
 		}
 	}
-	return tokens;
+	return tok;
 }
 
 ////////////////////////////////////////////////////////////////
-/* PARSE
- * the parser takes an array of tokens as an argument and
- * returns an AST in the form of nested expressions
- * Attention! parse() modifies the original array, so you'll
- * have to copy it first if you want to keep it for some reason
+/**
+ * PARSER
+ * Takes a list of tokens with the structure defined above,
+ * and arranges them in a lluna expression (~= s-expression)
+ *
+ * It checks the first token:
+ * - if it's an atom, return it
+ * - if it's an '(', return an expression and check recursively its content
+ * - if it's an '[', return a list and check each element
+ *
+ * @param {Array} tok - list of tokens
+ * @returns {(Object|Array)} t | expr - atom or AST of an expression
  */
+function parse(tok) {
+	let t = tok.shift();
 
-function parse(tokens) {
-	let t = tokens.shift();
-	// strings, numbers and keywords take the following structure:
-	// { type: X, val: Y, line: Z}
-	// everything else is just a string
-	if (t === '(') {
+	if (t.type === '(') {
+		// expression
 		let expr = [];
-		if (tokens[0] === ';') {
-			// a new line begins a multi-expression
-			tokens.shift(); // remove new line
-			let inner = []; // current inner expression
-			while (tokens[0] !== ')') {
-				if (tokens[0] == ';') {
-					// if there's tokens in 'inner',
-					// add them to 'expr' and clear 'inner'
-					if (inner.length > 0) expr.push(inner.splice(0));
-					tokens.shift(); // remove new line
+		if (tok[0].type === ';') {
+			// an opening parenthesis followed by a new line
+			// creates a multi-expression
+			tok.shift(); // remove new line
+			let innerExpr = [];
+
+			while (tok.length > 0 && tok[0].type !== ')') {
+				if (tok[0].type === ';') {
+					// if there's tokens in innerExpr, add them
+					// clearing innerExpr in the process
+					if (innerExpr.length > 0) expr.push(innerExpr.splice(0));
+					tok.shift(); // remove new line
 				} else {
-					inner.push(parse(tokens));
+					innerExpr.push(parse(tok));
 				}
-				if (tokens.length === 0) break;
 			}
-			// add last 'inner' to 'expr' if necessary
-			if (inner.length > 0) expr.push(inner.splice(0));
+			// add the last expression if necessary
+			if (innerExpr.length > 0) expr.push(innerExpr.splice(0));
 		} else {
-			// is a single expression
-			while (tokens[0] !== ')') {
-				if (tokens[0] === ';') {
-					tokens.shift(); // ignore new lines
+			// single expression
+			while (tok.length > 0 && tok[0].type !== ')') {
+				if (tok[0].type === ';') {
+					tok.shift(); // ignore new lines
 				} else {
-					expr.push(parse(tokens));
+					expr.push(parse(tok));
 				}
-				if (tokens.length === 0) break;
 			}
 		}
 
-		if (tokens[0] !== ')') {
-			throw '[!] Missing closing parenthesis.';
-		}
-		tokens.shift(); // remove ')'
+		if (tok.length === 0) throw `[!] Unclosed expression starting at line ${t.line}.`;
+		if (tok[0].type !== ')') throw `[!] Expected closing parenthesis around line ${tok[0].line}.`;
+
+		tok.shift(); // remove ')'
 		return expr;
-	} else if (t === ')') {
-		throw '[!] Unexpected closing parenthesis.';
+	} else if (t.type === '[') {
+		// TODO
+		/*
+		 *
+		 *
+		 *
+		 *
+		 */
+	} else if (t.type === ')') {
+		throw `[!] Unexpected closing parenthesis at line ${t.line}.`;
+	} else if (t.type === ']') {
+		throw `[!] Unexpected closing square bracket at line ${t.line}.`;
 	}
-	// is an atom
+	// atom
 	return t;
 }
 
@@ -141,9 +178,7 @@ function find(atom, env) {
 	for (let i = env.length - 1; i >= 0; i--) {
 		if (atom.val in env[i]) return env[i][atom.val];
 	}
-	console.log(
-		`[*] Empty variable '${atom.val}' at line ${atom.line}.\n    'null' will be returned instead.`
-	);
+	console.log(`[*] Empty variable '${atom.val}' at line ${atom.line}.\n    'null' will be returned instead.`);
 	return null;
 }
 
